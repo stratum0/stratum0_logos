dd180ea701726a348deaf09070475306c0a3fcde
Sebastian Willenborg <comawill@wlnbrg.de>
2014-10-02 20:51:20 +0200

    Stratum 0 Logos eingepflegt

---
+++ Stratum0/README.md
@@ -0,0 +1,10 @@
+# Stratum 0 Logos
+![Stratum 0](Stratum0_logo.png?raw=true)
+![Stratum 0](Stratum0_avatar.png?raw=true)
+![Stratum 0](Stratum0_avatar_black.png?raw=true)
+
+## Quelle
+https://stratum0.org/wiki/Logo
+
+## Lizenz
+Der Copyrightinhaber, @blinry, erlaubt freie Verwendung für alles und jeden. Die einzige Bedingung ist, dass man ihn auf Nachfrage als Urheber nennt.
---
+++ generate.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+DEFAULT_WIDTH_PDF=1024
+DEFAULT_WIDTH_PNG=500
+
+for dir in $(ls -d */);
+do
+	for svg in $(ls ${dir}*.svg);
+	do
+		fn=$(basename $svg)
+		base="${dir}${fn%.*}"
+		echo $base
+		inkscape --file "$svg" --export-png "${base}.png" --export-width ${DEFAULT_WIDTH_PNG} --export-background-opacity 0
+		inkscape --file "$svg" --export-pdf "${base}.pdf" --export-width ${DEFAULT_WIDTH_PNG}
+	done;
+done;
